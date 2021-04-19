2f1661207526476e199bf5e9d95a5464a81df23c
tekac <soehnerj@gmail.com>
2021-04-19 15:11:25 -0400

    learned debugging

--- developer_skills/script.js
+++ developer_skills/script.js
@@ -51,14 +51,14 @@
 
 // ///Problem 2
 
-// const calcTempAmplitudeNew = function (t1, t2) {
-//   const t3 = t1.concat(t2);
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
 
-//   let max = t3[0];
-//   let min = t3[0];
+//   let max = temps[0];
+//   let min = temps[0];
 
-//   for (let i = 0; i < t3.length; i++) {
-//     const curTemp = t3[i];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
 //     if (typeof curTemp !== 'number') continue;
 
 //     if (curTemp > max) max = curTemp;
@@ -67,13 +67,16 @@
 //   return max - min;
 // };
 
-// const amplitudeNew = calcTempAmplitudeNew(temperatures, temps2);
-// console.log(amplitudeNew);
+// const amplitudeBug = calcTempAmplitudeBug(temperatures, temps2);
+// console.log(amplitudeBug);
 const measureKelvin = function () {
   const measurement = {
     type: 'temp',
     unit: 'celsius',
-    value: prompt('Degrees celsius:'),
+
+    //C) FIX
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
   };
 
   //B) FIND THE BUG
@@ -88,3 +91,26 @@ const measureKelvin = function () {
 
 //A) IDENTIFY
 console.log(measureKelvin());
+
+//Using a debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
