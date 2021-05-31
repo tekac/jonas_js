ae3eab19cb8fe21730cda5ce959c86828063e919
tekac <soehnerj@gmail.com>
2021-05-31 02:15:07 -0400

    finished pig game

--- pig-game/script.js
+++ pig-game/script.js
@@ -14,16 +14,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
 
 // Starting Conditions //
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -68,7 +80,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 15) {
       //Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -84,17 +96,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-// Reset the Game
-
-btnNew.addEventListener('click', function () {
-  currentScore = 0;
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  if (activePlayer === 1) {
-    switchPlayer();
-  }
-});
+btnNew.addEventListener('click', init);
